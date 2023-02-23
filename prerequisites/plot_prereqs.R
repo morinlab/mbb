2971d24d8133991b4fa7cb91492b1cdd81d09b2a
Ryan Morin <rmorin@d207-023-223-159.wireless.sfu.ca>
2023-02-22 16:43:28 -0800

    updates

--- prerequisites/plot_prereqs.R
+++ prerequisites/plot_prereqs.R
@@ -1,5 +1,6 @@
 full_dag <- dagify(#MBB309 ~ MBB231,
                      MBB308 ~ MBB331,
+                     MBB322 ~ MBB231,
                      #MBB321 ~ MBB231,
                      MBB323 ~ MBB222,
                      MBB324 ~ MBB222,
@@ -59,4 +60,36 @@ ggdag(full_dag,layout="auto",node_size=15,text_size=2.5) + theme_dag_blank()
 ggdag(full_dag,layout="auto",node_size=14,text_size=2.2,edge_type = "diagonal") + theme_dag_blank() + ggtitle("Upper Division MBB")
 
 
+
+
+ii_dag <- dagify(
+  CHEM281 ~ CHEM121 + CHEM122,
+  MBB231 ~ MBB222 + CHEM282 + BISC101 + MBB222,
+  MBB222 ~ CHEM281,
+  MBB309 ~ MBB231 + CHEM215 + CHEM286,
+  MBB308 ~ MBB331,
+  MBB321 ~ MBB231,
+  MBB322 ~ MBB231,
+  MBB323 ~ MBB222 + MATH152 + CHEM122,
+  MBB324 ~ MBB222,
+  MBB326 ~ MBB231,
+  MBB328 ~ MBB231,
+  MBB331 ~ MBB231,
+  MBB342 ~ MBB110 + BISC202 + MBB110 + STAT270,
+  MBB402 ~ MBB331,
+  HSCI442 ~ MBB326 + MBB308,
+  MBB423 ~ MBB323,
+  MBB422 ~ MBB322 + MBB309,
+  MBB430 ~ MBB331 + MBB322,
+  MBB432 ~ MBB331 + MBB308,
+  MBB438 ~ MBB331,
+  MBB441 ~ MBB331 + MBB342,
+  MBB445 ~ MBB326 + MBB328,
+  MBB446 ~ MBB322 + MBB331,
+  MBB461 ~ MBB331 + MBB342,
+  MBB462 ~ MBB331 + MBB342,
+  MBB478 ~ MBB331
+)
+ggdag(ii_dag,layout="auto",node_size=15,text_size=2.5,edge_type = "diagonal") + 
+  theme_dag_blank() + ggtitle("Infection & Immunity Concentration")
 
